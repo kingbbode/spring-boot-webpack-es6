b21ddec64a7f7367b3061749b0615a68e5f778f0
kingbbode <kingbbode@gmail.com>
2016-11-17 23:14:18 +0900

    RequestMapping Annotation to GetMapping Annotation

--- src/main/java/com/kingbbode/controller/HomeController.java
+++ src/main/java/com/kingbbode/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.kingbbode.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String home(Model model){
         model.addAttribute("message","hello");
         return "index";
